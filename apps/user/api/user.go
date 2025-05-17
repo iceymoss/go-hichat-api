a11361fba2eca2c0784f5eb2f164d1164f801972
iceymoss <ice_moss@163.com>
2025-05-17 12:36:45 +0800

    ci: added a microservice startup script

--- apps/user/api/user.go
+++ apps/user/api/user.go
@@ -14,7 +14,7 @@ import (
 	"github.com/zeromicro/go-zero/rest"
 )
 
-var configFile = flag.String("f", "etc/local/user.yaml", "the config file")
+var configFile = flag.String("f", "etc/user-local.yaml", "the config file")
 
 func main() {
 	flag.Parse()
