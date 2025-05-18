0baa6d11064ab9689027e95aff6e5c095dd66012
iceymoss <ice_moss@163.com>
2025-05-18 20:52:23 +0800

    feat: added chat conversationId complete the most basic version of chat

---
+++ apps/im/ws/im.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/iceymoss/go-hichat-api/apps/im/ws/internal/handler"
+	pkcCfg "github.com/iceymoss/go-hichat-api/pkg/config"
+
+	"github.com/iceymoss/go-hichat-api/apps/im/ws/internal/config"
+	"github.com/iceymoss/go-hichat-api/apps/im/ws/internal/svc"
+	websocketServer "github.com/iceymoss/go-hichat-api/apps/im/ws/websocket"
+	"github.com/zeromicro/go-zero/core/conf"
+)
+
+var configFile = flag.String("f", "apps/im/ws/etc/im-local.yaml", "the config file")
+
+func main() {
+	flag.Parse()
+
+	//加载全局配置
+	pkcCfg.InitConfig("local", "", "config")
+
+	var c config.Config
+	conf.MustLoad(*configFile, &c)
+
+	if err := c.SetUp(); err != nil {
+		panic(err)
+	}
+
+	ctx := svc.NewServiceContext(c)
+
+	// 实例化websocket服务
+	srv := websocketServer.NewServer(c.ListenOn,
+		websocketServer.WithAuthentication(handler.NewJwtAuto(ctx)))
+	defer srv.Stop()
+
+	// 处理处理方法
+	handler.RegisterHandlers(srv, ctx)
+
+	fmt.Printf("Starting websocket server at %v ...\n", c.ListenOn)
+	srv.Start()
+}
