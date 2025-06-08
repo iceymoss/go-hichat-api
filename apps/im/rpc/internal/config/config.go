19d7c6386efe67cbcfcd386b0bb6906af06c0537
iceymoss <ice_moss@163.com>
2025-06-08 14:35:52 +0800

    test: test conversation api for im rpc

--- apps/im/rpc/internal/config/config.go
+++ apps/im/rpc/internal/config/config.go
@@ -4,4 +4,7 @@ import "github.com/zeromicro/go-zero/zrpc"
 
 type Config struct {
 	zrpc.RpcServerConf
+
+	SocialRpc zrpc.RpcClientConf //连接rpc服务的，通过服务发现etcd去获取到rpc服务的配置
+
 }
--- apps/im/rpc/internal/logic/creategroupconversationlogic.go
+++ apps/im/rpc/internal/logic/creategroupconversationlogic.go
@@ -2,10 +2,15 @@ package logic
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	models "github.com/iceymoss/go-hichat-api/apps/im/models"
+	"github.com/iceymoss/go-hichat-api/apps/social/rpc/social"
 	"github.com/iceymoss/go-hichat-api/pkg/constants"
+	zLog "github.com/iceymoss/go-hichat-api/pkg/logger"
 	"github.com/iceymoss/go-hichat-api/pkg/xerr"
 	"github.com/zeromicro/go-zero/core/errorx"
+	"go.uber.org/zap"
 
 	"github.com/iceymoss/go-hichat-api/apps/im/rpc/im"
 	"github.com/iceymoss/go-hichat-api/apps/im/rpc/internal/svc"
@@ -27,7 +32,7 @@ func NewCreateGroupConversationLogic(ctx context.Context, svcCtx *svc.ServiceCon
 	}
 }
 
-// CreateGroupConversation 创建群会话,创建群时创建
+// CreateGroupConversation 创建群会话,创建群时创建，群的会话是由创建群的时候创建，在会话表中只有一条群的会话，通过用户会话表进行绑定
 func (l *CreateGroupConversationLogic) CreateGroupConversation(in *im.CreateGroupConversationReq) (*im.CreateGroupConversationResp, error) {
 	res := &im.CreateGroupConversationResp{}
 
@@ -37,8 +42,25 @@ func (l *CreateGroupConversationLogic) CreateGroupConversation(in *im.CreateGrou
 		return res, nil
 	}
 
-	if err != models.ErrNotFound {
-		return nil, errorx.Wrapf(xerr.NewDBErr(), "find conversation err %v, req %v", err, in)
+	// 获取群学校，判断是否为群主
+	groupList, err := l.svcCtx.Social.GroupList(l.ctx, &social.GroupListReq{UserId: in.CreateId})
+	if err != nil {
+		zLog.Error("CreateGroupConversation.GroupList: get group users failed", zap.Any("req", in), zap.Error(err))
+		return nil, err
+	}
+
+	fmt.Printf("group:%+v\n", groupList)
+
+	isGroupCreater := false
+	for _, group := range groupList.List {
+		if group.CreatorUid == in.CreateId && group.Id == in.GroupId {
+			isGroupCreater = true
+			break
+		}
+	}
+
+	if !isGroupCreater {
+		return nil, errors.New("该用户不是群主")
 	}
 
 	err = l.svcCtx.ConversationModel.Insert(l.ctx, &models.Conversation{
@@ -46,6 +68,7 @@ func (l *CreateGroupConversationLogic) CreateGroupConversation(in *im.CreateGrou
 		ChatType:       constants.GroupChatType,
 	})
 	if err != nil {
+		zLog.Error(fmt.Sprintf("CreateGroupConversation.Insert err %v, req %v", err, in), zap.Error(err))
 		return res, errorx.Wrapf(xerr.NewDBErr(), "insert conversation err %v, req %v", err, in)
 	}
 
