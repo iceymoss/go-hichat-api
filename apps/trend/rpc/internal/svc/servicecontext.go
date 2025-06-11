5cd048695e61bc6c7df72d3528722570d0bb0679
iceymoss <ice_moss@163.com>
2025-06-11 22:24:10 +0800

    feat: run trend server

--- apps/trend/rpc/internal/svc/servicecontext.go
+++ apps/trend/rpc/internal/svc/servicecontext.go
@@ -1,13 +1,25 @@
 package svc
 
-import "github.com/iceymoss/go-hichat-api/apps/trend/rpc/internal/config"
+import (
+	"github.com/iceymoss/go-hichat-api/apps/trend/models"
+	"github.com/iceymoss/go-hichat-api/apps/trend/rpc/internal/config"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
 
 type ServiceContext struct {
 	Config config.Config
+
+	Trend models.TrendModel
+
+	TrendDiscuss models.TrendDiscussModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	sqlConn := sqlx.NewMysql(c.Mysql.DataSource)
+
 	return &ServiceContext{
-		Config: c,
+		Config:       c,
+		Trend:        models.NewTrendModel(sqlConn, c.Cache),
+		TrendDiscuss: models.NewTrendDiscussModel(sqlConn, c.Cache),
 	}
 }
