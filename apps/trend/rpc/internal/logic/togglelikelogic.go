b12dd3b8b5398aeb9ecce88ad513ea8e5cfd2cfc
iceymoss <ice_moss@163.com>
2025-06-21 21:19:45 +0800

    fix: handling likes and unlikes

--- apps/trend/rpc/internal/logic/togglelikelogic.go
+++ apps/trend/rpc/internal/logic/togglelikelogic.go
@@ -26,11 +26,19 @@ func NewToggleLikeLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Toggle
 
 // ToggleLike 点赞/取消点赞
 func (l *ToggleLikeLogic) ToggleLike(in *trend.LikeToggleRequest) (*trend.LikeToggleResponse, error) {
-	// todo: add your logic here and delete this line
-
 	err := l.svcCtx.TrendAgree.AgreeInc(l.ctx, uint64(in.UserId), uint64(in.AuthorId), uint64(in.TrendId), int(in.LikeType))
 	if err != nil {
 		return nil, fmt.Errorf("用户: %d 点赞动态: %d 失败: %s", in.UserId, in.TrendId, err)
+	}
+
+	// 动态总点赞数处理
+	incType := 0
+	if in.LikeType > 0 {
+		incType = 1
+	}
+	err = l.svcCtx.Trend.IncAgreeOrReply(l.ctx, uint64(in.TrendId), 0, incType)
+	if err != nil {
+		return nil, fmt.Errorf("更新动态点赞总数失败: trend_id: %d error: %s", in.TrendId, err)
 	}
 
 	return &trend.LikeToggleResponse{
