9cc4613f0ce8447d88782a8aa0e60545f2d3b682
iceymoss <ice_moss@163.com>
2025-06-29 17:01:05 +0800

    feat: added trend comment api

--- apps/trend/api/internal/logic/comment/getdiscussestreelogic.go
+++ apps/trend/api/internal/logic/comment/getdiscussestreelogic.go
@@ -2,6 +2,11 @@ package comment
 
 import (
 	"context"
+	"github.com/iceymoss/go-hichat-api/apps/trend/rpc/trend"
+	"github.com/iceymoss/go-hichat-api/apps/user/rpc/user"
+	zLog "github.com/iceymoss/go-hichat-api/pkg/logger"
+	"go.uber.org/zap"
+	"strconv"
 
 	"github.com/iceymoss/go-hichat-api/apps/trend/api/internal/svc"
 	"github.com/iceymoss/go-hichat-api/apps/trend/api/internal/types"
@@ -15,7 +20,7 @@ type GetDiscussesTreeLogic struct {
 	svcCtx *svc.ServiceContext
 }
 
-// 获取评论树
+// NewGetDiscussesTreeLogic 获取评论树
 func NewGetDiscussesTreeLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetDiscussesTreeLogic {
 	return &GetDiscussesTreeLogic{
 		Logger: logx.WithContext(ctx),
@@ -24,8 +29,67 @@ func NewGetDiscussesTreeLogic(ctx context.Context, svcCtx *svc.ServiceContext) *
 	}
 }
 
-func (l *GetDiscussesTreeLogic) GetDiscussesTree(req *types.GetDiscussesReq) (resp *types.DiscussesTreeResp, err error) {
-	// todo: add your logic here and delete this line
+func (l *GetDiscussesTreeLogic) GetDiscussesTree(req *types.GetDiscussesListReq) (resp *types.DiscussesTreeResp, err error) {
+	discuss, err := l.svcCtx.Trend.GetTrendDiscusses(l.ctx, &trend.GetDiscussesListReq{
+		TrendId: req.TrendID,
+		LastId:  int32(req.LastID),
+	})
+	if err != nil {
+		zLog.Error("获取动态失败", zap.Any("req", req), zap.Error(err))
+		return nil, err
+	}
+
+	// 获取用户信息
+	userInfo, err := l.svcCtx.User.FindUser(l.ctx, &user.FindUserReq{
+		Ids: discuss.UserIds,
+	})
+	if err != nil {
+		zLog.Error("获取用户信息失败", zap.Any("userIds", discuss.UserIds), zap.Error(err))
+		return nil, nil
+	}
+
+	userBind := make(map[string]*types.User, len(userInfo.User))
+	for _, v := range userInfo.User {
+		userBind[v.Id] = &types.User{
+			Id:       v.Id,
+			Nickname: v.Nickname,
+			Sex:      int(v.Sex),
+			Avatar:   v.Avatar,
+		}
+	}
+
+	res := types.GetDiscussesListResp{
+		DiscussesMap: map[uint64]types.TrendDiscusses{},
+	}
+	for k, v := range discuss.Discusses {
+		if _, ok := res.DiscussesMap[uint64(k)]; !ok {
+			res.DiscussesMap[uint64(k)] = types.TrendDiscusses{
+				Discusses: make([]*types.Discuss, 0),
+			}
+		}
+		// 一级评论
+		discussTemp := res.DiscussesMap[uint64(k)]
+		for _, dis := range v.List {
+			// 处理二级评论
+			seDisTemp := make([]*types.Discuss, 0, len(dis.Children))
+			for _, secDis := range dis.Children {
+				secDisTemp := discuss2Resp(secDis)
+				secDisTemp.Replyer = userBind[strconv.Itoa(int(secDis.Replyer))]
+				secDisTemp.User = userBind[strconv.Itoa(int(secDis.UserId))]
+				seDisTemp = append(seDisTemp, secDisTemp)
+			}
+			rootTemp := discuss2Resp(dis)
+			rootTemp.Children = seDisTemp
+			rootTemp.Replyer = userBind[strconv.Itoa(int(dis.Replyer))]
+			rootTemp.User = userBind[strconv.Itoa(int(dis.UserId))]
+			discussTemp.Discusses = append(discussTemp.Discusses, rootTemp)
+		}
+		res.DiscussesMap[uint64(k)] = discussTemp
+	}
+
+	resp = &types.DiscussesTreeResp{
+		Discusses: res,
+	}
 
 	return
 }
