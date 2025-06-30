c2f8edc7bfbddb40e3b6d323981c849de732cc39
iceymoss <ice_moss@163.com>
2025-06-30 21:50:04 +0800

    feat: complete the development of the comment function api

--- apps/trend/api/internal/handler/like/togglelikehandler.go
+++ apps/trend/api/internal/handler/like/togglelikehandler.go
@@ -9,7 +9,7 @@ import (
 	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
-// 点赞/取消点赞
+// ToggleLikeHandler 点赞/取消点赞
 func ToggleLikeHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req types.LikeToggleRequest
