e30632c2e64ea5333d1c2e1155b7ca8cf959df20
iceymoss <ice_moss@163.com>
2025-07-03 22:48:49 +0800

    fix: dealing with spanning issues

--- apps/user/api/user.go
+++ apps/user/api/user.go
@@ -22,10 +22,12 @@ func main() {
 	var c config.Config
 	conf.MustLoad(*configFile, &c)
 
-	server := rest.MustNewServer(c.RestConf)
+	server := rest.MustNewServer(c.RestConf, rest.WithCors("*"))
 	defer server.Stop()
 
 	ctx := svc.NewServiceContext(c)
+	//server.Use(middleware.CORSMiddleware())
+
 	handler.RegisterHandlers(server, ctx)
 
 	httpx.SetErrorHandlerCtx(http.ErrHandler(c.Name))
